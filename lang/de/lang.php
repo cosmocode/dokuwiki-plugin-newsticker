b1cc1cd8392ecc552312649b2cd36aaaa19b40c5
Michael Große <grosse@cosmocode.de>
2015-10-22 14:11:59 +0200

    Add German translation

---
+++ lang/de/lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * English language file for newsticker plugin
+ *
+ * @author Michael Große <grosse@cosmocode.de>
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Your menu entry';
+
+// custom language strings for the plugin
+$lang['previous'] = 'zurück';
+$lang['next'] = 'vorwärts';
+
+
+
+//Setup VIM: ex: et ts=4 :
---
+++ lang/de/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * english language file for newsticker plugin
+ *
+ * @author Michael Große <grosse@cosmocode.de>
+ */
+
+// keys need to match the config setting name
+$lang['duration'] = 'Anzeigedauer einer Nachricht';
+
+
+
+//Setup VIM: ex: et ts=4 :
