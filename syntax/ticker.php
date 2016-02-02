35913ace37bab05b7305884267644109428cb0c6
Andreas Gohr <andi@splitbrain.org>
2016-02-02 16:12:05 +0100

    Adjust method signatures to match parent

--- syntax/ticker.php
+++ syntax/ticker.php
@@ -52,7 +52,7 @@ class syntax_plugin_newsticker_ticker extends DokuWiki_Syntax_Plugin {
      * @param Doku_Handler    $handler The handler
      * @return array Data for the renderer
      */
-    public function handle($match, $state, $pos, Doku_Handler &$handler){
+    public function handle($match, $state, $pos, Doku_Handler $handler){
         if ($state !== 3) {
             return array();
         }
@@ -90,7 +90,7 @@ class syntax_plugin_newsticker_ticker extends DokuWiki_Syntax_Plugin {
      * @param array          $data      The data from the handler() function
      * @return bool If rendering was successful.
      */
-    public function render($mode, Doku_Renderer &$renderer, $data) {
+    public function render($mode, Doku_Renderer $renderer, $data) {
         if($mode != 'xhtml') return false;
         if (empty($data)) return false;
 
